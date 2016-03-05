b8937db1e966bb51f0e83e70ab21ae026b05a2e4
DongHee Kim <opklnm102@gmail.com>
2016-03-05 20:26:09 +0900

    UrlConncetion hint 추가

--- MyApplication/app/src/main/java/kr/ac/hansung/myapplication/webcon.java
+++ MyApplication/app/src/main/java/kr/ac/hansung/myapplication/webcon.java
@@ -9,6 +9,15 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by Owner on 2016-02-22.
  */
@@ -48,13 +57,13 @@ public class webcon extends Activity {
     }
 
 
-    class BackgroundTask extends AsyncTask<Integer , Integer , Integer> {
+    class BackgroundTask extends AsyncTask<Integer, Integer, Integer> {
         protected void onPreExecute() {
             value = 0;
             progress.setProgress(value);
         }
 
-        protected Integer doInBackground(Integer ... values) {
+        protected Integer doInBackground(Integer... values) {
             while (isCancelled() == false) {
                 value++;
                 if (value >= 100) {
@@ -63,15 +72,56 @@ public class webcon extends Activity {
                     publishProgress(value);
                 }
 
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {}
-            }
+                //Todo: http request
 
+                /*   example.
+                     이런 형식으로 할것 참고로 밑에 코드는 try-catch를 막붙인것 이므로 정리해서 쓸것
+
+
+                 URL url = null;
+
+                    url = new URL("www.naver.com");
+
+                HttpURLConnection urlConnection = null;
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+
+                    urlConnection.setRequestMethod("GET");
+
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+                    urlConnection.connect();
+
+                InputStream is = null;
+
+                    is = urlConnection.getInputStream();
+
+                if (is == null) {
+                    return;
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+                String line;
+                StringBuffer buffer = new StringBuffer();
+
+                    while ((line = reader.readLine()) != null) {
+                        while ((line = reader.readLine()) != null) {
+                            buffer.append(line);
+                        }
+
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException ex) {
+                        }
+                    }
+
+                 */
+            }
             return value;
         }
 
-        protected void onProgressUpdate(Integer ... values) {
+        protected void onProgressUpdate(Integer... values) {
             progress.setProgress(values[0].intValue());
             textView01.setText("Current Value : " + values[0].toString());
         }
