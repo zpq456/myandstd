02a4e2b2e42e5faeada5dd0513809fc6e59fc492
Woo Jae Yoo <zpq456@naver.com>
2016-03-08 10:23:01 +0900

    #2 PhotoGallery 시작

---
+++ MyApplication/potogallery/src/main/java/kr/ac/hansung/potogallery/PhotoGallery.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.potogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+}
